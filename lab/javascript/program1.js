a2ed58e30196dc65986be37a79a43463f39fa531
manthan <manthanmoharana03583@gmail.com>
2022-07-05 20:45:28 +0530

    First Commit

---
+++ lab/javascript/program1.js
@@ -0,0 +1,52 @@
+function show(){
+    // alert("Welcome to to World of javascript");
+    // document.write("Welcome to the world of Java");
+
+    var roll= "19btcse18";
+    var address = "Niladrivihar";
+    var Hobby = "Playing Monoply";
+
+    // document.writeln("My roll.no is s"+roll+"<br>");
+    // document.writeln("My Address is "+address+"<br>");
+    // document.writeln("My Hobby is "+Hobby);
+    // alert("My roll.no is s"+roll+"<br>");
+    // alert("My Address is "+address+"<br>");
+    // alert("My Hobby is "+Hobby);
+    var add = 3+4;
+
+    alert("3 + 4 = "+add);
+}
+
+var a1 = 10;
+        function abc(){
+            var a2 = 20;
+            document.write(a1+"<br>");
+            document.write(a2);
+        }
+
+        document.write(a2);
+
+function ari(){
+    n1 = 8;
+    n2 = 5;
+
+    add = n1+n2;
+
+    alert("8+5 = "+ (n1+n2));
+    alert("8-5 = "+ (n1-n2));
+    alert("8x5 = "+ (n1*n2));
+    alert("8/5 = "+ (n1/n2));
+    alert("8x5 = "+ (n1%n2));
+    alert("8++"+ (n1++));
+    n1=8;
+    alert("5-- = "+ (n2--));
+    n2 = 5
+    alert("8**5 = "+ (n1**n2));
+}
+
+function showimage(){
+    src = "background.jpg";
+    alert("press ok to see the image");
+    document.write("<img width=1000 height=1000 src="+src+"></img>");
+}
+
