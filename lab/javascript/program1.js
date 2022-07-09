843315cfc485397b5a0bef5a44f10a8e13480bf4
manthan <manthanmoharana03583@gmail.com>
2022-07-09 20:54:40 +0530

    adding files

--- lab/javascript/program1.js
+++ lab/javascript/program1.js
@@ -6,15 +6,15 @@ function show(){
     var address = "Niladrivihar";
     var Hobby = "Playing Monoply";
 
-    // document.writeln("My roll.no is s"+roll+"<br>");
-    // document.writeln("My Address is "+address+"<br>");
-    // document.writeln("My Hobby is "+Hobby);
+    document.writeln("My roll.no is s"+roll+"<br>");
+    document.writeln("My Address is "+address+"<br>");
+    document.writeln("My Hobby is "+Hobby);
     // alert("My roll.no is s"+roll+"<br>");
     // alert("My Address is "+address+"<br>");
     // alert("My Hobby is "+Hobby);
-    var add = 3+4;
+    // var add = 3+4;
 
-    alert("3 + 4 = "+add);
+    // alert("3 + 4 = "+add);
 }
 
 var a1 = 10;
@@ -48,5 +48,14 @@ function showimage(){
     src = "background.jpg";
     alert("press ok to see the image");
     document.write("<img width=1000 height=1000 src="+src+"></img>");
+    
 }
 
+// function show(){
+//     // document.write("rambit");
+//     // document.write("19btcse32"
+    
+//     console.log("Rambit");
+//     console.log("19btcse32");
+// }
+// show();
